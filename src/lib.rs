b4fb426f664ac8447535303c77fec509209bf408
Nils Sohn <sohn.cloud@proton.me>
2023-12-17 22:56:47 -0700

    Project dependencies have been added.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
 mod utils;
 
 use crate::utils::set_panic_hook;
-use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, Nils!");
+    print!("Hello, Nils!");
 }
 
-#[wasm_bindgen]
 pub fn init() {
     set_panic_hook();
 }
