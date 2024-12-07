e7325d91ea14479645efefcd91d4ad08f256ee31
sohndotcloud <sohn.cloud@proton.me>
2024-12-07 00:35:20 -0700

    Updated script to include rust installer curl

--- setup.sh
+++ setup.sh
@@ -3,12 +3,23 @@
 
 if ! command -v rustc 2>&1 >/dev/null
 then
-    echo "Rustc could not be found. Please install rust with rustup."
-    echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
-    echo
-    exit 1
-fi
+    echo "Rust compiler could not be found."
 
+    while true; do
+    read -p "Do you want to install rust toolchain? (Y/n) " yn
+    case $yn in
+            [yY] )  echo Running Rustup curl command;
+                    break;;
+            [nN] )  
+                    echo Exiting setup.;
+                    echo ;
+                    echo Rust can be installed with:;
+                    echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh";
+                    exit;;
+            * ) echo Invalid Response;;
+    esac
+    done
+fi
 
 cargo install trunk wasm-bindgen-cli
 
